09c778a23ce9ee461b45f10db2c4acb8416f972f
adhan-mattos <129184651+adhan-mattos@users.noreply.github.com>
2024-08-27 08:38:16 -0300

    rttr

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const caixaPergunta = document.querySelector(".caixa-pergunta");
 const caixaAlternativa = document.querySelector(".caixa-alernativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto_resultado");
+
+let exemplo = "uma questão de pergunta";
+let exemplo2 = 23;
+let exemplo3 = false;
+let exemplo4 = true;
